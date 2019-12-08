2cb306f77fabc22dbd08efa83b8992116303ce25
waspryo <wasp2123321@gmail.com>
2019-12-08 12:34:13 +0900

    show all comments

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+Vue.component('comment', {
+    props: {
+        postId: Number,
+        id: Number,
+        name: String,
+        email: String,
+        body: String,
+    },
+    template: `
+      <div class="comment">
+        <h1>{{ name }}</h1>
+        <p>{{ body }}</p>
+      </div>
+    `
+})
+
 const app = new Vue({
     el: '#app',
     // インスタンスにアクセスできる
